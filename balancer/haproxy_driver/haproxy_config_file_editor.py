6c0fa29c495758ab3944350383e30b7468f19959
sgalkin <sgalkin@3b54a22f-fe44-4ad7-b819-32ae0e9b6c2a>
2012-03-16 12:29:32 +0000

    add haproxy_project
    git-svn-id: svn://192.168.19.148/OpenstackLB/trunk/balancer@8 3b54a22f-fe44-4ad7-b819-32ae0e9b6c2a

---
+++ balancer/haproxy_driver/haproxy_config_file_editor.py
@@ -0,0 +1,13 @@
+class HaproxyConfigFile(object):
+    """
+    A class for manipulate haproxy configuration file 
+    """
+    def __init__(self,  haproxy_config_file_path):
+        self.haproxy_config_file_path=haproxy_config_file_path
+        print self.haproxy_config_file_path
+    
+    
+    
+
+
+    
