a2e59b8a83e63a858ae74bc6ca20bb8e5463f988
sgalkin <sgalkin@3b54a22f-fe44-4ad7-b819-32ae0e9b6c2a>
2012-03-16 13:04:05 +0000

    ***
    git-svn-id: svn://192.168.19.148/OpenstackLB/trunk/balancer@11 3b54a22f-fe44-4ad7-b819-32ae0e9b6c2a

--- balancer/haproxy_driver/haproxy_config_file_editor.py
+++ balancer/haproxy_driver/haproxy_config_file_editor.py
@@ -1,13 +1,17 @@
-class HaproxyConfigFile(object):
-    """
-    A class for manipulate haproxy configuration file 
-    """
+# -*- coding: utf-8 -*-
+
+import os
+
+class HaproxyConfigFile():
     def __init__(self,  haproxy_config_file_path):
-        self.haproxy_config_file_path=haproxy_config_file_path
-        print self.haproxy_config_file_path
+        self.haproxy_config_file_path = haproxy_config_file_path
+        
     
+    def get_filename (self):
+        return self.haproxy_config_file_path
     
-    
-
-
+if __name__ == '__main__':
+	haproxyconfigfile = ("bla-bla")
+	filename = haproxyconfigfile.get_filename()
+	print filename
     
