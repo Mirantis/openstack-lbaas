57940b9c5c3e274dcc630941e102e6fa198db0ae
ibumarskov <ibumarskov@3b54a22f-fe44-4ad7-b819-32ae0e9b6c2a>
2012-03-19 13:00:52 +0000

    Added trunk folder
    git-svn-id: svn://192.168.19.148/OpenstackLB/trunk/balancer@27 3b54a22f-fe44-4ad7-b819-32ae0e9b6c2a

---
+++ balancer/drivers/acedriver.py
@@ -0,0 +1,37 @@
+import re
+
+class acedriver():
+    def __init__():
+        pass
+
+	def add (dict):
+		if self.dict == "rs_name":
+			checkproperty_name(self.dict)
+		if self.dict == "ip":
+			checkproperty_ip(self.dict)
+		if self.dict == "sfarm_name":
+			checkproperty_name(self.dict)
+		# after checking necessary parametrs create rserver and serverfarm
+		
+		if not checkInDB(self.rs_name):
+			create_rserv()
+		
+	def checkproperty_name(name):
+		if re.match (name, "regexpress"):
+			return 1
+		else:
+			return "Wrong name"
+
+	def create_rserv():
+        
+
+class lb():
+	pass
+	lb.rs_name = "RS01"
+	lb.rs_ip = "10.1.1.1"
+    lb.sfarm
+    lb.description = "rfddttgrg"
+
+r = acedriver()
+r.add(rsObject)
+
