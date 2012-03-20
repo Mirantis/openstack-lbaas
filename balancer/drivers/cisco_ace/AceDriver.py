817c792aaf4b6cb4cdd5d2a70ccfb8535980c333
timnur <timnur@3b54a22f-fe44-4ad7-b819-32ae0e9b6c2a>
2012-03-20 16:02:16 +0000

    Rename AceDriver class, modified AceDriver and BaseDriver classes
    git-svn-id: svn://192.168.19.148/OpenstackLB/trunk/balancer@59 3b54a22f-fe44-4ad7-b819-32ae0e9b6c2a

--- balancer/drivers/cisco_ace/AceDriver.py
+++ balancer/drivers/cisco_ace/AceDriver.py
@@ -1,12 +1,29 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2012 OpenStack LLC.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
 class acedriver(BaseDriver):
-    def init(self):
+    def __init__(self):
         pass
     
-    def addRealServer(self, obj):
-        TMP="<RealServer>\n"
-        TMP=TMP+"rsever "+obj.type+" "+obj.name+"\n"
+    def createRServer(self, obj):
+        TMP="<rserver "
+        TMP=TMP+"type='"+obj.type+"' name='"+obj.name+"'>\n"
         if obj.type == "host":
-            TMP=TMP+"ip address "+obj.IP+"\n"
+            TMP=TMP+"<ip address node='address' ipv4-address='"+obj.IP+"'/>\n"
             if obj.failOnAll != None: TMP=TMP+"fail-on-all\n"
         TMP=TMP+"conn-limit max "+str(obj.maxCon)+" min "+str(obj.minCon)+"\n"
         TMP=TMP+"weight "+str(obj.weight)+"\n"
@@ -21,19 +38,14 @@ class acedriver(BaseDriver):
             TMP=TMP+obj.state+"\n"
         for i in range(len(obj.probes)):
             TMP=TMP+"probe "+obj.probes[i]+"\n"
-        TMP=TMP+"<\\RealServer>\n"
+        TMP=TMP+"<inservice/></rserver>\n"
         return TMP
 
-    def modReaslServer(self, obj):
-        pass
-
-    def delRealServer(self, obj):
-        TMP="<RealServer>\n"
-        TMP=TMP+"no rsever "+obj.name+"\n"
-        TMP=TMP+"<\\RealServer>\n"
+    def deleteRServer(self, obj):
+        TMP="<rserver sense='no' type='host' name='" + obj.name + "'></rserver>\n"
         return TMP
 
-    def addSFarm(self, obj):
+    def createServerFarm(self, obj):
         TMP="<SFarm>\n"
         TMP=TMP+"rsever "+obj.type+" "+obj.name+"\n"
         for i in range(len(obj.probes)):
@@ -64,10 +76,10 @@ class acedriver(BaseDriver):
         TMP=TMP+"predictor"+obj.predictor #Check it !!!
         TMP="<\\SFarm>\n"
         
-    def SFarmaddRS(self, obj):
+    def addRServerToSF(self, obj):
         TMP="<RealServer>\n"
         pass
     
-    def SFarmdelRS(self, obj):
+    def deleteRServerFromSF(self, obj):
         pass
         
