c54d655034c9107a0920aa312a62ebff36a49cb3
akyrenys <akyrenys@3b54a22f-fe44-4ad7-b819-32ae0e9b6c2a>
2012-03-20 17:05:22 +0000

    git-svn-id: svn://192.168.19.148/OpenstackLB/trunk/balancer@62 3b54a22f-fe44-4ad7-b819-32ae0e9b6c2a

--- etc/tables.sql
+++ etc/tables.sql
@@ -16,4 +16,14 @@ CREATE TABLE transactions (id INT, status TEXT, action TEXT, params TEXT);
 
 CREATE TABLE devices (id INT,  name  TEXT, type TEXT, version TEXT, supports_IPv6 INT, require_VIP_IP INT, has_ACL INT, supports_VLAN INT);
 
-CREATE TABLE vips(id INT, ip_type TEXT, ip_addr TEXT, transport_proto TEXT, app_proto TEXT, port INT, vlan INT, sf_id INT, backup_sf_id INT);
+#CREATE TABLE vips(id INT, ip_type TEXT, ip_addr TEXT, transport_proto TEXT, app_proto TEXT, port INT, vlan INT, sf_id INT, backup_sf_id INT);
+CREATE TABLE vips(ICMPreply TEXT, KALAPprimaryOutOfServ TEXT, KALAPtagName TEXT, L7LoadBalancing TEXT, NAT TEXT, Port TEXT, SSLinitiation TEXT, 
+        SSLproxyServName TEXT, VLAN TEXT, allVLANs TEXT, appAccelAndOpt TEXT, appProto TEXT, backupServerFarm TEXT, connParameterMap TEXT, created TEXT, defaultL7LBAction TEXT, 
+        id INT, ip TEXT, ipType TEXT, name TEXT, proto TEXT, protocolInspect TEXT, serverFarm TEXT, status TEXT, sf_id INT, updated TEXT, virtIPmask TEXT);
+
+
+CREATE TABLE vlans(ARPinspectType TEXT, IPaddr TEXT, MTU TEXT, RPF TEXT, UDPconfigCommands TEXT, actionForDBfit TEXT, actionForIPheaderOptions TEXT,
+        adminStatus TEXT, aliasIPaddr TEXT, description TEXT, enableDHCPrelay TEXT, enableICMPguard TEXT, enableIPv6 TEXT, enableMACAddrAutogen TEXT,
+        enableMACsticky TEXT, enableNormalization TEXT, enableSynCookieThreshValue TEXT, id INT, inputAccessGroup TEXT, inputPolicies TEXT, intType TEXT,
+        ipv6GlobalIP TEXT, ipv6LinkLocalAddr TEXT, ipv6PeerLinkLocalAddr TEXT, ipv6UniqueLocalAddr TEXT, maxFragChainsAllowed TEXT, minFragMTUvalue TEXT, 
+        minTTLipHeaderValue TEXT, netmask TEXT, outputAccessGroup TEXT, peerIPaddr TEXT, reassemblyTimeout TEXT, secondaryIPgroups TEXT);
