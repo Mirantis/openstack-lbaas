e33c4f538b554b354c70443d3fb1bd9e6cc4c339
akyrenys <akyrenys@3b54a22f-fe44-4ad7-b819-32ae0e9b6c2a>
2012-03-20 14:41:39 +0000

    git-svn-id: svn://192.168.19.148/OpenstackLB/trunk/balancer@50 3b54a22f-fe44-4ad7-b819-32ae0e9b6c2a

---
+++ balancer/loadbalancers/virtualserver.py
@@ -0,0 +1,192 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2012 OpenStack LLC.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import logging
+import vlan
+
+class VirtualServer(object):
+    def __init__(self):
+        self._id = None
+        self._name = ""
+        self._ipType = "IPv4"
+        self._ip = ""
+        self._virtIPmask = "255.255.255.255"
+        self._proto = "TCP"
+        self._appProto = "Other"
+        self._Port = "any"
+        self._allVLANs = None
+        self._VLAN = [] #need to describe in new module
+        self._connParameterMap = None #need to describe in new module
+        self._KALAPtagName = ""
+        self._KALAPprimaryOutOfServ = None
+        self._ICMPreply = "None"
+        self._status = "In Service"
+        
+        self._protocolInspect = None #need to describe in new module
+        self._appAccelAndOpt = None #need to describe in new module
+        self._L7LoadBalancing = None #need to describe in new module
+        self._SSLproxyServName = None #need to describe in new module
+        self._defaultL7LBAction = None #need to describe in new module
+        self._SSLinitiation = None #need to describe in new module
+        self._NAT = [] #need to describe in new module
+        self._created = None
+        self._updated = None
+    
+    @property
+    def id(self):
+        return self._id 
+    @id.setter
+    def id(self, value):
+        self._id  = value
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, value):
+        self._name = value
+    @property
+    def ipType(self):
+        return self._ipType
+    @ipType.setter
+    def ipType(self, value):
+        self._ipType = value
+    @property
+    def ip(self):
+        return self._ip
+    @ip.setter
+    def ip(self, value):
+        self._ip = value
+    @property
+    def virtIPmask(self):
+        return self._virtIPmask
+    @virtIPmask.setter
+    def virtIPmask(self, value):
+        self._virtIPmask = value
+    @property
+    def proto(self):
+        return self._proto
+    @proto.setter
+    def proto(self, value):
+        self, _proto = value
+    @property
+    def appProto(self):
+        return self._appProto
+    @appProto.setter
+    def appProto(self, value):
+        self._appProto = value
+    @property
+    def Port(self):
+        return self._Port
+    @Port.setter
+    def Port(self, value):
+        self._Port = value
+    @property
+    def allVLANs(self):
+        return self._allVLANs
+    @allVLANs.setter
+    def allVLANs(self, value):
+        self._allVLANs = value
+    @property
+    def VLAN(self):
+        return self._VLAN
+    @VLAN.setter
+    def VLAN(self, value):
+        self._VLAN = value
+    @property
+    def connParameterMap(self):
+        return self._connParameterMap
+    @connParameterMap.setter
+    def connParameterMap(self, value):
+        self._connParameterMap = value
+    @property
+    def KALAPtagName(self):
+        return self._KALAPtagName
+    @KALAPtagName.setter
+    def KALAPtagName(self, value):
+        self._KALAPtagName = value
+    @property
+    def KALAPprimaryOutOfServ(self):
+        return self._KALAPprimaryOutOfServ
+    @KALAPprimaryOutOfServ.setter
+    def KALAPprimaryOutOfServ(self, value):
+        self._KALAPprimaryOutOfServ = value
+    @property
+    def ICMPreply(self):
+        return self._ICMPreply
+    @ICMPreply.setter
+    def ICMPreply(self, value):
+        self._ICMPreply = value
+    @property
+    def status(self):
+        return self._status
+    @status.setter
+    def status(self, value):
+        self._status = value
+    @property
+    def protocolInspect(self):
+        return self._protocolInspect
+    @protocolInspect.setter
+    def protocolInspect(self, value):
+        self._protocolInspect = value
+    @property
+    def appAccelAndOpt(self):
+        return self._appAccelAndOpt
+    @appAccelAndOpt.setter
+    def appAccelAndOpt(self, value):
+        self._appAccelAndOpt = value
+    @property
+    def L7LoadBalancing(self):
+        return self._L7LoadBalancing
+    @L7LoadBalancing.setter
+    def L7LoadBalancing(self, value):
+        self._L7LoadBalancing = value
+    @property
+    def SSLproxyServName(self):
+        return self._SSLproxyServName
+    @SSLproxyServName.setter
+    def SSLproxyServName(self, value):
+        self._SSLproxyServName = value
+    @property
+    def defaultL7LBAction(self, value):
+        return self._defaultL7LBAction
+    @defaultL7LBAction.setter
+    def defaultL7LBAction(self, value):
+        self._defaultL7LBAction = value
+    @property
+    def SSLinitiation(self):
+        return self._SSLinitiation
+    @SSLinitiation.setter
+    def SSLinitiation(self, value):
+        self._SSLinitiation = value
+    @property
+    def NAT(self):
+        return self._NAT
+    @NAT.setter
+    def NAT(self, value):
+        self._NAT = value
+    @property
+    def created(self):
+        return self._created
+    @created.setter
+    def created(self, value):
+        self._created = value
+    @property
+    def updated(self):
+        return self._updated
+    @updated.setter
+    def updated(self, value):
+        self._updated = value
---
+++ balancer/loadbalancers/vlan.py
@@ -0,0 +1,255 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2012 OpenStack LLC.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import logging
+
+class VLAN(object):
+    def __init__(self):
+        self._id = None
+        self._description = ""
+        self._intType = "Routed"
+        self._IPaddr = ""
+        self._aliasIPaddr = ""
+        self._peerIPaddr = ""
+        self._netmask = ""
+        self._adminStatus = "Up"
+        self._enableMACsticky = None
+        self._enableNormalization = "IPv4v6"
+        self._enableIPv6 = None
+        self._ipv6GlobalIP = [] #possible use as ["IPv6Address","eui-64","aliasIPv6Address","peerIPv6Address","eui-64","prefixLength"]
+        #                                                          ["2002:1:2:3::1","0","2002:2:3:4::1","2002:3:4:5::1","0","80"]
+        self._ipv6UniqueLocalAddr = [] #possible use as ["IPv6Address","eui-64","peerIPv6Address","eui-64","prefixLenght"]
+        #                                                                        ["fb8:22:22::1","0","fb8:23:23::1","0","64"]
+        self._ipv6LinkLocalAddr = ""
+        self._ipv6PeerLinkLocalAddr = ""
+        self._enableICMPguard = "IPv4v6"
+        self._enableDHCPrelay = ""# "Ipv4v6" or "IPv4" or "IPv6"
+        self._RPF = ""
+        self._reassemblyTimeout = ["5", "60"] # ["ipv4","ipv6"]
+        self._maxFragChainsAllowed = ["24", "24"] 
+        self._minFragMTUvalue = ["576", "1280"]
+        self._MTU = ["1500", "1500"]
+        self._actionForIPheaderOptions = ["Clear-Invalid", "Drop"]
+        self._enableMACAddrAutogen = None
+        self._minTTLipHeaderValue = ""
+        self._enableSynCookieThreshValue = ""
+        self._actionForDBfit = "Allow"
+        self._ARPinspectType = None
+        self._UDPconfigCommands = None
+        self._secondaryIPgroups = [] # ["IP","aliasIP","peerIP","netmask"]
+        self._inputPolicies = []
+        self._inputAccessGroup = []
+        self._outputAccessGroup = []
+        
+    @property
+    def id(self):
+        return self._id
+    @id.setter
+    def id(self, value):
+        self._id = value
+    @property
+    def description(self):
+        return self._description
+    @description.setter
+    def description(self, value):
+        self._description = value
+    @property
+    def intType(self):
+        return self._intType
+    @intType.setter
+    def intType(self, value):
+        self._intType = value
+    @property
+    def IPaddr(self):
+        return self._IPaddr
+    @IPaddr.setter
+    def IPaddr(self, value):
+        self._IPaddr = value
+    @property
+    def aliasIPaddr(self):
+        return self._aliasIPaddr
+    @aliasIPaddr.setter
+    def aliasIPaddr(self, value):
+        self._aliasIPaddr = value
+    @property
+    def peerIPaddr(self):
+        return self._peerIPaddr
+    @peerIPaddr.setter
+    def peerIPaddr(self, value):
+        self._peerIPaddr = value
+    @property
+    def netmask(self):
+        return self._netmask
+    @netmask.setter
+    def netmask(self, value):
+        self._netmask = value
+    @property
+    def adminStatus(self):
+        return self._adminStatus
+    @adminStatus.setter
+    def adminStatus(self, value):
+        self._adminStatus =value
+    @property
+    def enableMACsticky(self):
+        return self._enableMACsticky
+    @enableMACsticky.setter
+    def enableMACsticky(self, value):
+        self._enableMACsticky = value
+    @property
+    def enableNormalization(self):
+        return self._enableNormalization
+    @enableNormalization.setter
+    def enableNormalization(self, value):
+        self._enableNormalization = value
+    @property
+    def enableIPv6(self):
+        return self._enableIPv6
+    @enableIPv6.setter
+    def enableIPv6(self, value):
+        self._enableIPv6 = value
+    @property
+    def ipv6GlobalIP(self):
+        return self._ipv6GlobalIP
+    @ipv6GlobalIP.setter
+    def ipv6GlobalIP(self, value):
+        self._ipv6GlobalIP = value
+    @property
+    def ipv6UniqueLocalAddr(self):
+        return self._ipv6UniqueLocalAddr
+    @ipv6UniqueLocalAddr.setter
+    def ipv6UniqueLocalAddr(self, value):
+        self._ipv6UniqueLocalAddr = value
+    @property
+    def ipv6LinkLocalAddr(self):
+        return self._ipv6LinkLocalAddr
+    @ipv6LinkLocalAddr.setter
+    def ipv6LinkLocalAddr(self, value):
+        self._ipv6LinkLocalAddr = value
+    @property
+    def ipv6PeerLinkLocalAddr(self):
+        return self._ipv6PeerLinkLocalAddr
+    @ipv6PeerLinkLocalAddr.setter
+    def ipv6PeerLinkLocalAddr(self, value):
+        self._ipv6PeerLinkLocalAddr = value
+    @property
+    def enableICMPguard(self):
+        return self._enableICMPguard
+    @enableICMPguard.setter
+    def enableICMPguard(self, value):
+        self._enableICMPguard = value
+    @property
+    def enableDHCPrelay(self):
+        return self._enableDHCPrelay
+    @enableDHCPrelay.setter
+    def enableDHCPrelay(self, value):
+        self._enableDHCPrelay = value
+    @property
+    def RPF(self):
+        return self._RPF
+    @RPF.setter
+    def RPF(self, value):
+        self._RPF = value
+    @property
+    def reassemblyTimeout(self):
+        return self._reassemblyTimeout
+    @reassemblyTimeout.setter
+    def reassemblyTimeout(self, value):
+        self._reassemblyTimeout = value
+    @property
+    def maxFragChainsAllowed(self):
+        return self._maxFragChainsAllowed
+    @maxFragChainsAllowed.setter
+    def maxFragChainsAllowed(self, value):
+        self._maxFragChainsAllowed = value
+    @property
+    def minFragMTUvalue(self):
+        return self._minFragMTUvalue
+    @minFragMTUvalue.setter
+    def minFragMTUvalue(self, value):
+        self._minFragMTUvalue = value
+    @property
+    def MTU(self):
+        return self._MTU
+    @MTU.setter
+    def MTU(self, value):
+        self._MTU = value
+    @property
+    def actionForIPheaderOptions(self):
+        return self._actionForIPheaderOptions
+    @actionForIPheaderOptions.setter
+    def actionForIPheaderOptions(self, value):
+        self._actionForIPheaderOptions = value
+    @property
+    def enableMACAddrAutogen(self):
+        return self._enableMACAddrAutogen
+    @enableMACAddrAutogen.setter
+    def enableMACAddrAutogen(self, value):
+        self._enableMACAddrAutogen = value
+    @property
+    def minTTLipHeaderValue(self):
+        return self._minTTLipHeaderValue
+    @minTTLipHeaderValue.setter
+    def minTTLipHeaderValue(self, value):
+        self._minTTLipHeaderValue = value
+    @property
+    def enableSynCookieThreshValue(self):
+        return self._enableSynCookieThreshValue
+    @enableSynCookieThreshValue.setter
+    def enableSynCookieThreshValue(self, value):
+        self._enableSynCookieThreshValue = value
+    @property
+    def actionForDBfit(self):
+        return self._actionForDBfit
+    @actionForDBfit.setter
+    def actionForDBfit(self, value):
+        self._actionForDBfit = value
+    @property
+    def ARPinspectType(self):
+        return self._ARPinspectType
+    @ARPinspectType.setter
+    def ARPinspectType(self, value):
+        self._ARPinspectType = value
+    @property
+    def UDPconfigCommands(self):
+        return self._UDPconfigCommands
+    @UDPconfigCommands.setter
+    def UDPconfigCommands(self, value):
+        self._UDPconfigCommands = value
+    @property
+    def secondaryIPgroups(self):
+        return self._secondaryIPgroups
+    @secondaryIPgroups.setter
+    def secondaryIPgroups(self, value):
+        self._secondaryIPgroups = value
+    @property
+    def inputPolicies(self):
+        return self._inputPolicies
+    @inputPolicies.setter
+    def inputPolicies(self, value):
+        self._inputPolicies = value
+    @property
+    def inputAccessGroup(self):
+        return self._inputAccessGroup
+    @inputAccessGroup.setter
+    def inputAccessGroup(self, value):
+        self._inputAccessGroup = value
+    @property
+    def outputAccessGroup(self):
+        return self._outputAccessGroup
+    @outputAccessGroup.setter
+    def outputAccessGroup(self, value):
+        self._outputAccessGroup = value
