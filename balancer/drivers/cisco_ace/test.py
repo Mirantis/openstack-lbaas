d8fba25c51d63b9bf3a699d3bb47be7a174f2092
ibumarskov <ibumarskov@3b54a22f-fe44-4ad7-b819-32ae0e9b6c2a>
2012-03-21 15:41:48 +0000

    Modify
    git-svn-id: svn://192.168.19.148/OpenstackLB/trunk/balancer@77 3b54a22f-fe44-4ad7-b819-32ae0e9b6c2a

--- balancer/drivers/cisco_ace/test.py
+++ balancer/drivers/cisco_ace/test.py
@@ -7,16 +7,23 @@ sys.path.append('balancer/loadbalancers')
 from AceDriver import AceDriver
 from Context import Context
 from realserver import RealServer
-
+from serverfarm import ServerFarm
 
 rs = RealServer()
 rs.name = 'Test_to_delete'
 rs.IP = '10.1.1.1'
 
+sf = ServerFarm()
+sf.name = "openstack_sfarm"
+sf.predictor = "roundrobin"
+sf.description = "description"
+
 test_context = Context('10.4.15.30', '10443', 'admin', 'cisco123')
 driver = AceDriver()
 print "||===  Creation The Real Server  ============||"
-print driver.createRServer(test_context, rs)
+#print driver.createRServer(test_context, rs)
 print "||===  Deletion The Real Server  ============||"
-print driver.deleteRServer(test_context, rs)
+#print driver.deleteRServer(test_context, rs)
+print "||===  Creation The Server Farm  ============||"
+print driver.createServerFarm(test_context, sf)
 print "||=============  Test Complite  =============||"
