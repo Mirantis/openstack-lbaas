b197a184f70b4556ffd28a62fc1286701392169c
timnur <timnur@3b54a22f-fe44-4ad7-b819-32ae0e9b6c2a>
2012-03-21 09:05:18 +0000

    Added XmlSender class for AceDriver
    git-svn-id: svn://192.168.19.148/OpenstackLB/trunk/balancer@67 3b54a22f-fe44-4ad7-b819-32ae0e9b6c2a

---
+++ balancer/drivers/cisco_ace/XmlSender.py
@@ -0,0 +1,48 @@
+import urllib2
+import base64
+import re 
+
+url = "https://10.4.15.30:10443/bin/xml_agent"
+username = "admin"
+password = "cisco123"
+
+r = urllib2.Request(url)
+
+base64string = base64.encodestring(
+                '%s:%s' % (username, password))[:-1]
+authheader =  "Basic %s" % base64string
+r.add_header("Authorization", authheader)
+
+data = """xml_cmd=<request_xml>
+%s
+</request_xml>"""
+
+handle = urllib2.urlopen(r, data)
+print handle.geturl()
+t = handle.read()
+print t
+
+
+class XmlSender:
+    def __init__(self,  context):
+        self.url = "https://%s:%s/bin/xml_agent" % (context.ip, context.port)
+
+    def getParam(self, name):
+        return self._params.get(name,  None)
+    
+    def deployConfig(self,  context,  command):
+        request = urllib2.Request(self.url)
+        
+        base64str = base64.encodestring(
+                                        '%s:%s' % (context.login, context.password))[:-1]
+        
+        authheader = "Basic %s" % base64str
+        
+        request.add_header("Authorization", authheader)
+        
+        data = """xml_cmd<request_xml>
+        %s
+        </request_xml>""" % command
+        
+        massage = urllib2.urlopen(request, data)
+        return re.find(message.read(), 'XML_CMD_SUCCESS')
