d543dffc6f9b2ea6e4d22708d69289cdfeaf2a78
sgalkin <sgalkin@3b54a22f-fe44-4ad7-b819-32ae0e9b6c2a>
2012-03-21 16:37:32 +0000

    haproxy update
    git-svn-id: svn://192.168.19.148/OpenstackLB/trunk/balancer@81 3b54a22f-fe44-4ad7-b819-32ae0e9b6c2a

--- balancer/drivers/haproxy/haproxy_config_file_editor.py
+++ balancer/drivers/haproxy/haproxy_config_file_editor.py
@@ -1,44 +1,41 @@
 # -*- coding: utf-8 -*-
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
 
+# Copyright 2012 OpenStack LLC.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License
 
 import re
 import sys
 import os
-from balancer.loadbalancers.realserver import RealServer
+import shutil
 
-#class HaproxyRServer(VServerName):
-#   def _init_(self):
-#        
-#   def AddHaproxyRServer(self):
-#        
-#        
-#    def DeleteHaproxyRServer(self):
-#
-#class HaproxyVServer:
-#    def _init_(self):
-#        
-#    def CreateHaproxyVServer(self):
-    
-    
+
+  
 class HaproxyConfigFile:
-
-
-    def __init__(self):
-        self.haproxy_config_file_path = "/etc/haproxy/haproxy.cfg"
-        self.haproxy_config_file_tmp_path="/tmp/haproxy.cfg"
-        self.debug = False
-         
+    def __init__(self, haproxy_config_file_path = '/tmp/haproxy.cfg'):
+        test_config = '/home/sgalkin/src/balancer/balancer/tests/unit/testfiles/haproxy.cfg'
+        shutil.copyfile (test_config, "/tmp/haproxy.cfg")
+        self.haproxy_config_file_path = haproxy_config_file_path
     def GetHAproxyConfigFileName(self):
         return self.haproxy_config_file_path
-    
     def DeleteListenBlock (self,  ListenBlockName):
         self.haproxy_config_file = open (self.haproxy_config_file_path,  "r")
         block_start=False
         new_config_file = []
-        #print ListenBlockName
         for line in  self.haproxy_config_file :
             if  not line.strip(): continue
-            if  line.find ('#') >= 0: continue
             if line.find('listen' ) == 0 and  line.find(ListenBlockName) > 0:
                 block_start = True
                 continue
@@ -47,12 +44,11 @@ class HaproxyConfigFile:
             elif  line.find('listen' ) == 0 and block_start == True:
                 block_start = False
             new_config_file.append(line.rstrip())
-            #print  line.rstrip()
         self.haproxy_config_file.close()
-        self.haproxy_config_file_tmp  = open (self.haproxy_config_file_tmp_path,  "w")
+        self.haproxy_config_file = open (self.haproxy_config_file_path,  "w")
         for out_line in new_config_file:
-            self.haproxy_config_file_tmp.write("%s\n" % out_line)
-        self.haproxy_config_file_tmp.close()
+            self.haproxy_config_file.write("%s\n" % out_line)
+        self.haproxy_config_file.close()
         return ListenBlockName
  
     def DeleteRServer (self, ListenBlockName, RServerName):
@@ -60,41 +56,70 @@ class HaproxyConfigFile:
         block_start=False
         new_config_file = []
         for line in  self.haproxy_config_file :
-            print line.find(RServerName),  line.find('server'),  block_start
             if  not line.strip(): continue
-            if  line.find ('#') >= 0: continue
             if line.find('listen' ) == 0 and  line.find(ListenBlockName) > 0:
                 block_start = True
                 new_config_file.append(line.rstrip())
                 continue
             elif line.find('server') >= 0 and block_start == True and line.find(RServerName) > 0:
-                print "bingo"
                 continue
             elif  line.find('listen' ) == 0 and block_start == True:
                 block_start = False
             new_config_file.append(line.rstrip())
             #print  line.rstrip()
         self.haproxy_config_file.close()
-        self.haproxy_config_file_tmp  = open (self.haproxy_config_file_tmp_path,  "w")
+        self.haproxy_config_file  = open (self.haproxy_config_file_path,  "w")
         for out_line in new_config_file:
-            self.haproxy_config_file_tmp.write("%s\n" % out_line)
-        self.haproxy_config_file_tmp.close()
+            self.haproxy_config_file.write("%s\n" % out_line)
+        self.haproxy_config_file.close()
+        return ListenBlockName
+    
+    def AddRServer (self,  ListenBlockName, RServerName, RServerIP,  RServerPort):
+        self.haproxy_config_file = open (self.haproxy_config_file_path,  "r")
+        block_start=False
+        new_config_file = []
+        for line in  self.haproxy_config_file :
+            if  not line.strip(): continue
+            if line.find('listen' ) == 0 and  line.find(ListenBlockName) > 0:
+                block_start = True
+                new_config_file.append(line.rstrip())
+                continue
+            elif  line.find('listen' ) == 0 and block_start == True:
+                new_config_file.append("\tserver\t%s %s:%s check inter 2000 rise 2 fall 5" % (RServerName, RServerIP,  RServerPort) )
+                block_start = False
+            new_config_file.append(line.rstrip())
+            #print  line.rstrip()
+        self.haproxy_config_file.close()
+        self.haproxy_config_file  = open (self.haproxy_config_file_path,  "w")
+        for out_line in new_config_file:
+            self.haproxy_config_file.write("%s\n" % out_line)
+        self.haproxy_config_file.close()
+        return ListenBlockName       
+        
+    def AddListenBlock(self,  ListenBlockName,  VIPServerIP,  VIPServerPort):
+        self.haproxy_config_file = open (self.haproxy_config_file_path,  "r")
+        new_config_file = []
+        for line in  self.haproxy_config_file :
+            if  not line.strip(): continue
+            new_config_file.append(line.rstrip())
+        self.haproxy_config_file.close()
+        new_config_file.append("listen %s %s:%s" % (ListenBlockName, VIPServerIP,  VIPServerPort))
+        new_config_file.append("\tmode http")
+        new_config_file.append("\tbalance roundrobin")
+        new_config_file.append("\toption httpclose")
+        self.haproxy_config_file  = open (self.haproxy_config_file_path,  "w")
+        for out_line in new_config_file:
+            self.haproxy_config_file.write("%s\n" % out_line)
+        self.haproxy_config_file.close()
         return ListenBlockName
 
-        
-        
-    #def CreateVIP(self, name,  ip, port,  mode,  balance,  option,  reservers):
-        
-    
-    
-
-
-    
- 
 if __name__ == '__main__':
     config = HaproxyConfigFile()
-    #config.DeleteListenBlock("appli1-rewrite")
-    config.DeleteRServer("appli1-rewrite", "app1_2" )
+    config.DeleteListenBlock("appli2-insert")
+    #config.DeleteRServer("appli1-rewrite", "app1_2" )
+    #config.AddRServer("appli1-rewrite", "new_server", "1.1.1.1", "80"  )
+    #config.AddListenBlock("new_block", "12.12.12.12", "80" )
+    
     
     
     
--- balancer/tests/unit/test_haproxy_driver.py
+++ balancer/tests/unit/test_haproxy_driver.py
@@ -2,21 +2,23 @@
 
 from balancer.drivers.haproxy.haproxy_config_file_editor import  HaproxyConfigFile
 import unittest
+import os
+import shutil
+import filecmp
 
 
 class HAproxyDriverTestCase (unittest.TestCase):
-
-
-    def test_FileName(self):
-        filename = HaproxyConfigFile()
-        self.assertEqual(filename.GetHAproxyConfigFileName(),  "/etc/haproxy/haproxy.cfg")
-      
-    def test_DeleteListenBlock (self):
-        test = HaproxyConfigFile()
-        self.assertEqual(test.DeleteListenBlock("appli1-rewrite"), 'appli1-rewrite')
+    def setUp (self):
+        shutil.copyfile ('./balancer/tests/unit/testfiles/haproxy.cfg',  "/tmp/haproxy.cfg")
         
-
-
+    def test_FileName(self):
+        filename = HaproxyConfigFile("/tmp/haproxy.cfg")
+        self.assertEqual(filename.GetHAproxyConfigFileName(),  "/tmp/haproxy.cfg")
+    def test_DeleteListenBlock (self):
+        test = HaproxyConfigFile("/tmp/haproxy.cfg")
+        test.DeleteListenBlock("appli2-insert")
+        self.assertTrue(filecmp.cmp("/tmp/haproxy.cfg", "./balancer/tests/unit/testfiles/haproxy_without_appli2-insert.cfg"))
+ 
 
 if __name__ == "__main__":
 	unittest.main()
