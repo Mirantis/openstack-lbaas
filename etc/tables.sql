9f89ee0fb5aa70de564493089f0981d6f62cf4e0
akyrenys <akyrenys@3b54a22f-fe44-4ad7-b819-32ae0e9b6c2a>
2012-03-21 09:19:34 +0000

    git-svn-id: svn://192.168.19.148/OpenstackLB/trunk/balancer@69 3b54a22f-fe44-4ad7-b819-32ae0e9b6c2a

--- etc/tables.sql
+++ etc/tables.sql
@@ -2,14 +2,14 @@ CREATE TABLE loadbalancers(id INT, name TEXT, algorithm TEXT, status TEXT, creat
 
 CREATE TABLE serverfarms(id INT, lb_id INT,  name TEXT,
          type TEXT,  description TEXT, failAction TEXT, inbandHealthCheck TEXT, connFailureThreshCount TEXT,  resetTimeout TEXT,   resumeService TEXT,  transparent TEXT, 
-         dynamicWorkloadScale TEXT,  vmProbeName TEXT, failOnAll TEXT,  partialThreshPercentage TEXT,   backInservice TEXT,   predictor_id INT,  retcodeMap TEXT,   status TEXT,    created TEXT,
-         updated TEXT);
+         dynamicWorkloadScale TEXT,  vmProbeName TEXT, failOnAll TEXT,  partialThreshPercentage TEXT,   backInservice TEXT, probes TEXT , rservers TEXT, predictor_id INT,  
+         retcodeMap TEXT,   status TEXT,    created TEXT,  updated TEXT);
 
 CREATE TABLE predictors( id INT, name TEXT, type TEXT);
 
-CREATE TABLE rservers(id INT,   name TEXT,
+CREATE TABLE rservers(id INT, sf_id INT, name TEXT,
          type TEXT, webHostRedir TEXT,   ipType TEXT,   IP TEXT,  port TEXT,  state TEXT,   opstate TEXT,    description TEXT,   failOnAll TEXT,   minCon INT,  maxCon INT, 
-         weight INT,  rateBandwidth INT,   rateConn INT,    created TEXT,  updated TEXT, sf_id INT);
+         weight INT,  probes TEXT, rateBandwidth INT,   rateConn INT,    created TEXT,  updated TEXT);
 
 
 CREATE TABLE transactions (id INT, status TEXT, action TEXT, params TEXT);
@@ -17,13 +17,14 @@ CREATE TABLE transactions (id INT, status TEXT, action TEXT, params TEXT);
 CREATE TABLE devices (id INT,  name  TEXT, type TEXT, version TEXT, supports_IPv6 INT, require_VIP_IP INT, has_ACL INT, supports_VLAN INT);
 
 #CREATE TABLE vips(id INT, ip_type TEXT, ip_addr TEXT, transport_proto TEXT, app_proto TEXT, port INT, vlan INT, sf_id INT, backup_sf_id INT);
-CREATE TABLE vips(ICMPreply TEXT, KALAPprimaryOutOfServ TEXT, KALAPtagName TEXT, L7LoadBalancing TEXT, NAT TEXT, Port TEXT, SSLinitiation TEXT, 
-        SSLproxyServName TEXT, VLAN TEXT, allVLANs TEXT, appAccelAndOpt TEXT, appProto TEXT, backupServerFarm TEXT, connParameterMap TEXT, created TEXT, defaultL7LBAction TEXT, 
-        id INT, ip TEXT, ipType TEXT, name TEXT, proto TEXT, protocolInspect TEXT, serverFarm TEXT, status TEXT, sf_id INT, updated TEXT, virtIPmask TEXT);
+CREATE TABLE vips(id INT, sf_id INT, name TEXT, ipType TEXT, ip TEXT, virtIPmask TEXT, proto TEXT, appProto TEXT, Port TEXT, allVLANs TEXT, VLAN TEXT,
+        connParameterMap  TEXT,  KALAPtagName TEXT,  KALAPprimaryOutOfServ TEXT,  ICMPreply TEXT,  status TEXT,  protocolInspect TEXT,  appAccelAndOpt TEXT,
+        L7LoadBalancing TEXT,  serverFarm TEXT,  backupServerFarm TEXT,  SSLproxyServName TEXT,  defaultL7LBAction TEXT,  SSLinitiation TEXT,  NAT TEXT,
+        created TEXT,  updated TEXT);
 
 
-CREATE TABLE vlans(ARPinspectType TEXT, IPaddr TEXT, MTU TEXT, RPF TEXT, UDPconfigCommands TEXT, actionForDBfit TEXT, actionForIPheaderOptions TEXT,
-        adminStatus TEXT, aliasIPaddr TEXT, description TEXT, enableDHCPrelay TEXT, enableICMPguard TEXT, enableIPv6 TEXT, enableMACAddrAutogen TEXT,
-        enableMACsticky TEXT, enableNormalization TEXT, enableSynCookieThreshValue TEXT, id INT, inputAccessGroup TEXT, inputPolicies TEXT, intType TEXT,
-        ipv6GlobalIP TEXT, ipv6LinkLocalAddr TEXT, ipv6PeerLinkLocalAddr TEXT, ipv6UniqueLocalAddr TEXT, maxFragChainsAllowed TEXT, minFragMTUvalue TEXT, 
-        minTTLipHeaderValue TEXT, netmask TEXT, outputAccessGroup TEXT, peerIPaddr TEXT, reassemblyTimeout TEXT, secondaryIPgroups TEXT);
+CREATE TABLE vlans(id   INT, description   TEXT, intType   TEXT, IPaddr   TEXT, aliasIPaddr   TEXT, peerIPaddr   TEXT, netmask   TEXT, adminStatus   TEXT, 
+        enableMACsticky   TEXT, enableNormalization   TEXT, enableIPv6   TEXT, ipv6GlobalIP   TEXT, ipv6UniqueLocalAddr   TEXT, ipv6LinkLocalAddr   TEXT, 
+        ipv6PeerLinkLocalAddr   TEXT, enableICMPguard   TEXT, enableDHCPrelay   TEXT, RPF   TEXT, reassemblyTimeout   TEXT, maxFragChainsAllowed   TEXT, minFragMTUvalue   TEXT, 
+        MTU   TEXT, actionForIPheaderOptions   TEXT, enableMACAddrAutogen   TEXT, minTTLipHeaderValue   TEXT, enableSynCookieThreshValue   TEXT, actionForDBfit   TEXT, 
+        ARPinspectType   TEXT, UDPconfigCommands   TEXT, secondaryIPgroups   TEXT, inputPolicies   TEXT, inputAccessGroup   TEXT, outputAccessGroup   TEXT);
