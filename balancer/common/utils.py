0fed358d07260fda72b664ca4127a8857bafae81
gokrokve <gokrokve@3b54a22f-fe44-4ad7-b819-32ae0e9b6c2a>
2012-03-22 11:25:12 +0000

    Added workflow components 1. Added commands to worker. IT allows to undo operations 2. Added device.py for handling device related requests
    git-svn-id: svn://192.168.19.148/OpenstackLB/trunk/balancer@97 3b54a22f-fe44-4ad7-b819-32ae0e9b6c2a

---
+++ balancer/common/utils.py
@@ -0,0 +1,64 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2012 OpenStack LLC.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+class Singleton:
+    """
+    A non-thread-safe helper class to ease implementing singletons.
+    This should be used as a decorator -- not a metaclass -- to the
+    class that should be a singleton.
+
+    The decorated class can define one `__init__` function that
+    takes only the `self` argument. Other than that, there are
+    no restrictions that apply to the decorated class.
+
+    To get the singleton instance, use the `Instance` method. Trying
+    to use `__call__` will result in a `TypeError` being raised.
+
+    Limitations: The decorated class cannot be inherited from and the
+    type of the singleton instance cannot be checked with `isinstance`. 
+
+    """
+
+    def __init__(self, decorated):
+        self._decorated = decorated
+
+    def Instance(self):
+        """
+        Returns the singleton instance. Upon its first call, it creates a
+        new instance of the decorated class and calls its `__init__` method.
+        On all subsequent calls, the already created instance is returned.
+
+        """
+        try:
+            return self._instance
+        except AttributeError:
+            self._instance = self._decorated()
+            return self._instance
+
+    def __call__(self):
+        """
+        Call method that raises an exception in order to prevent creation
+        of multiple instances of the singleton. The `Instance` method should
+        be used instead.
+
+        """
+        raise TypeError(
+            'Singletons must be accessed through the `Instance` method.')
+
+
+## @singleton
