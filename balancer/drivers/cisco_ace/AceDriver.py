9d58fc249653c8fc3cb54399bc2008fbfdeab3e5
ibumarskov <ibumarskov@3b54a22f-fe44-4ad7-b819-32ae0e9b6c2a>
2012-03-22 12:20:41 +0000

    Modify
    git-svn-id: svn://192.168.19.148/OpenstackLB/trunk/balancer@104 3b54a22f-fe44-4ad7-b819-32ae0e9b6c2a

--- balancer/drivers/cisco_ace/AceDriver.py
+++ balancer/drivers/cisco_ace/AceDriver.py
@@ -66,63 +66,37 @@ class AceDriver(BaseDriver):
         
         s = XmlSender(context)
         return s.deployConfig(context, XMLstr)    
-
-
-
-
-    def createServerFarm(self,  context,  serverfarm):
-        if not bool(serverfarm.name):
-            return "SERVER FARM NAME ERROR"
-        
-        XMLstr = "<serverfarm type='" + serverfarm.type.lower() + "' name='" + serverfarm.name + "'>\r\n"
-        
-        if bool(serverfarm.description):
-            XMLstr = XMLstr + "<description descr-string='" + serverfarm.description + "'/> \r\n"
-        
-        if bool(serverfarm.failAction):
-            XMLstr = XMLstr + "<failaction failaction-type='" + serverfarm.failAction + "'/>\r\n"
-        
-        if bool(serverfarm.predictor): #Some predictors are may include additional parameters !
-            XMLstr = XMLstr + "<predictor predictor-method='" + serverfarm.predictor + "'/>\r\n"
-        
-        for i in range(len(serverfarm.probes)):
-            XMLstr = XMLstr + "<probe_sfarm probe-name='" + serverfarm.probes[i] + "'/>\r\n"
-        
-        if serverfarm.type.lower() == "host":
-            if bool(serverfarm.failOnAll): 
-                XMLstr = XMLstr + "<probe_sfarm probe-name='fail-on-all'/>\r\n"
-            
-            if bool(serverfarm.transparent):
-                XMLstr = XMLstr + "<transparent/>\r\n"
-            
-            if bool(serverfarm.partialThreshPercentage) and bool(serverfarm.backInservice):
-                XMLstr = XMLstr + "<partial-threshold value='" + serverfarm.partialThreshPercentage + "' back-inservice='" + serverfarm.backInservice + "'/>\r\n"
-            
-            if bool(serverfarm.inbandHealthCheck):
-                XMLstr = XMLstr + "<inband-health check='" + serverfarm.inbandHealthCheck + "'"
-                if serverfarm.inbandHealthCheck.lower == "log":
-                    XMLstr = XMLstr + "threshold='" + str(serverfarm.connFailureThreshCount) + "' reset='" + str(serverfarm.resetTimeout) + "'" #Do deploy if  resetTimeout='' ?
-                    
-                if serverfarm.inbandHealthCheck.lower == "remove":
-                    XMLstr=XMLstr+"threshold='"+str(serverfarm.connFailureThreshCount)+"' reset='"+str(serverfarm.resetTimeout)+"'  resume-service='"+str(serverfarm.resumeService)+"'" #Do deploy if  resumeService='' ?
-                XMLstr=XMLstr+"/>\r\n"
-            
-            if bool(serverfarm.dynamicWorkloadScale): # Need to upgrade (may include VM's)
-                XMLstr = XMLstr + "<dws type='" + serverfarm.failAction + "'/>\r\n"
-        
-        XMLstr = XMLstr + "</serverfarm>"
-        
-        res = XmlSender(context)
-        return res.deployConfig(context, XMLstr) 
-
     
-    def addRServerToSF(self, obj): 
-        TMP="<serverfarm name='"+obj.name+"'>\n"
-        for i in range(len(obj.rservers)):
-            TMP=TMP+"<rserver name="+obj.probes[i]+"\\\n"
-            TMP=TMP+"<inservice sense='inservice'\\>\n"
-        TMP=TMP+"<//serverfarm>\n"
-        return TMP
+    
+    def deleteRServer(self, context, rserver):
+        if not bool(rserver.name): 
+            return 'RSERVER NAME ERROR'
+        
+        XMLstr = "<rserver sense='no' name='" + rserver.name + "'></rserver>"
+        
+        s = XmlSender(context)
+        return s.deployConfig(context, XMLstr)  
+    
+    
+    def activateRServer(self,  context,  serverfarm,  rserver):
+        if not bool(rserver.name): 
+            return 'RSERVER NAME ERROR'
+
+        XMLstr = "<rserver name='" + rserver.name + "'>\r\n  <inservice/>\r\n</rserver>"
+        
+        s = XmlSender(context)
+        return s.deployConfig(context, XMLstr)
+    
+    
+    def suspendRServer(self,  context,  serverfarm,  rserver):
+        if not bool(rserver.name): 
+            return 'RSERVER NAME ERROR'
+
+        XMLstr = "<rserver name='" + rserver.name + "'>\r\n  <inservice sense='no'/>\r\n</rserver>"
+        
+        s = XmlSender(context)
+        return s.deployConfig(context, XMLstr)
+    
     
     def createProbe(self,  context,  probe):
         if not bool(probe.name): 
@@ -229,13 +203,157 @@ class AceDriver(BaseDriver):
             return "PROBE TYPE ERROR"
             
         s = XmlSender(context)
-        return s.deployConfig(context, XMLstr)  
-
+        return s.deployConfig(context, XMLstr)
     
-    def attachProbeToSF(self,  context,  serverfarm,  probe):
-        TMP="<serverfarm name='"+serverfarm.name+"'>\n"
-        TMP=TMP+"<probe name='"+probe.name+"'/>\n"
-        TMP=TMP+"</serverfarm>"
+    
+    def deleteProbe(self,  context,  probe):
+        pass
+    
+    
+    def createServerFarm(self,  context,  serverfarm):
+        if not bool(serverfarm.name):
+            return "SERVER FARM NAME ERROR"
+        
+        XMLstr = "<serverfarm type='" + serverfarm.type.lower() + "' name='" + serverfarm.name + "'>\r\n"
+        
+        if bool(serverfarm.description):
+            XMLstr = XMLstr + "<description descr-string='" + serverfarm.description + "'/> \r\n"
+        
+        if bool(serverfarm.failAction):
+            XMLstr = XMLstr + "<failaction failaction-type='" + serverfarm.failAction + "'/>\r\n"
+        
+        if bool(serverfarm.predictor): #Some predictors are may include additional parameters !
+            XMLstr = XMLstr + "<predictor predictor-method='" + serverfarm.predictor + "'/>\r\n"
+        
+        for i in range(len(serverfarm.probes)):
+            XMLstr = XMLstr + "<probe_sfarm probe-name='" + serverfarm.probes[i] + "'/>\r\n"
+        
+        if serverfarm.type.lower() == "host":
+            if bool(serverfarm.failOnAll): 
+                XMLstr = XMLstr + "<probe_sfarm probe-name='fail-on-all'/>\r\n"
+            
+            if bool(serverfarm.transparent):
+                XMLstr = XMLstr + "<transparent/>\r\n"
+            
+            if bool(serverfarm.partialThreshPercentage) and bool(serverfarm.backInservice):
+                XMLstr = XMLstr + "<partial-threshold value='" + serverfarm.partialThreshPercentage + "' back-inservice='" + serverfarm.backInservice + "'/>\r\n"
+            
+            if bool(serverfarm.inbandHealthCheck):
+                XMLstr = XMLstr + "<inband-health check='" + serverfarm.inbandHealthCheck + "'"
+                if serverfarm.inbandHealthCheck.lower == "log":
+                    XMLstr = XMLstr + "threshold='" + str(serverfarm.connFailureThreshCount) + "' reset='" + str(serverfarm.resetTimeout) + "'" #Do deploy if  resetTimeout='' ?
+                    
+                if serverfarm.inbandHealthCheck.lower == "remove":
+                    XMLstr=XMLstr+"threshold='"+str(serverfarm.connFailureThreshCount)+"' reset='"+str(serverfarm.resetTimeout)+"'  resume-service='"+str(serverfarm.resumeService)+"'" #Do deploy if  resumeService='' ?
+                XMLstr=XMLstr+"/>\r\n"
+            
+            if bool(serverfarm.dynamicWorkloadScale): # Need to upgrade (may include VM's)
+                XMLstr = XMLstr + "<dws type='" + serverfarm.failAction + "'/>\r\n"
+        
+        XMLstr = XMLstr + "</serverfarm>"
+        
+        res = XmlSender(context)
+        return res.deployConfig(context, XMLstr) 
+    
+    
+    def deleteServerFarm(self,  context,  serverfarm):
+        if not bool(serverfarm.name): 
+            return 'SERVER FARM NAME ERROR'
+
+        XMLstr = "<serverfarm sense='no' name='" + serverfarm.name + "'></serverfarm>"
+        
+        s = XmlSender(context)
+        return s.deployConfig(context, XMLstr) 
+    
+    
+    def addRServerToSF(self,  context,  serverfarm,  rserver): #rserver in sfarm may include many parameters !
+        if not bool(serverfarm.name) or not bool(rserver.name):
+            return "ERROR"
+        
+        XMLstr = "<serverfarm name='" + serverfarm.name + "'>\r\n"
+        XMLstr=XMLstr+" <rserver_sfarm name='"+rserver.name+"'"
+        if bool(rserver.port):
+            XMLstr=XMLstr+" port='"+rserver.port+"'"
+        XMLstr=XMLstr+">\r\n"
+        if bool(rserver.weight):
+            XMLstr=XMLstr+"<weight value='"+str(rserver.weight)+"'/>\r\n"
+        if bool(rserver.backupRS):
+            XMLstr=XMLstr+"<backup-rserver rserver-name='"+rserver.backupRS+"'"
+            if bool(rserver.backupRSport):
+                XMLstr=XMLstr+" port='"+rserver.backupRSport+"'"
+            XMLstr=XMLstr+"/>\r\n"
+        if bool(rserver.maxCon) and bool(rserver.minCon):
+            XMLstr=XMLstr+"<conn-limit max='"+str(rserver.maxCon)+"' min='"+str(rserver.minCon)+"'/>\r\n"
+        else:
+            XMLstr=XMLstr+"<conn-limit sense='no'/>\r\n"
+        if bool(rserver.rateConn):
+            XMLstr=XMLstr+"<rate-limit type='connection' value='"+str(rserver.rateConn)+"'/>\r\n"
+        if bool(rserver.rateBandwidth):
+            XMLstr=XMLstr+"<rate-limit type='bandwidth' value='"+str(rserver.rateBandwidth)+"'/>"
+       
+        
+        if rserver.state.lower() == "inservice":
+            XMLstr=XMLstr+"<inservice/>\r\n"
+        if rserver.state.lower() == "standby":
+            XMLstr=XMLstr+"<inservice mode='"+rserver.state.lower()+"'/>\r\n"
+        if rserver.state.lower() == "outofservice":
+            XMLstr=XMLstr+"<inservice sense='no'/>\r\n"
+        XMLstr=XMLstr+"</rserver_sfarm>\r\n"
+        XMLstr=XMLstr+"</serverfarm>"
+        
+        res = XmlSender(context)
+        return res.deployConfig(context, XMLstr)
+    
+    
+    def deleteRServerFromSF(self,  context,  serverfarm,  rserver):
+        if not bool(serverfarm.name) or not bool(rserver.name):
+            return "ERROR"
+        
+        XMLstr = "<serverfarm name='" + serverfarm.name + "'>\r\n"
+        XMLstr=XMLstr+" <rserver_sfarm sense='no' name='"+rserver.name+"'/>\r\n"
+        XMLstr=XMLstr+"</serverfarm>"
+        
+        XMLstr = "<serverfarm name='" + serverfarm.name + "'>\r\n"
+        XMLstr=XMLstr+" <rserver_sfarm name='"+rserver.name+"'"
+        if bool(rserver.port):
+            XMLstr=XMLstr+" port='"+rserver.port+"'"
+        XMLstr=XMLstr+">\r\n"
+        
+        res = XmlSender(context)
+        return res.deployConfig(context, XMLstr)
+    
+    
+    def addProbeToSF(self,  context,  serverfarm,  probe):
+        if not bool(serverfarm.name) or not bool(probe.name):
+            return "ERROR"
+        
+        XMLstr = "<serverfarm name='" + serverfarm.name + "'>\r\n"
+        XMLstr=XMLstr+" <probe_sfarm probe-name='"+probe.name+"'/>\r\n"
+        XMLstr=XMLstr+"</serverfarm>"
+        
+        res = XmlSender(context)
+        return res.deployConfig(context, XMLstr)
+    
+    
+    def deleteProbeFromSF (elf,  context,  serverfarm,  probe):
+        if not bool(serverfarm.name) or not bool(probe.name):
+            return "ERROR"
+        
+        XMLstr = "<serverfarm name='" + serverfarm.name + "'>\r\n"
+        XMLstr=XMLstr+" <probe_sfarm sense='no' probe-name='"+probe.name+"'/>\r\n"
+        XMLstr=XMLstr+"</serverfarm>"
+        
+        res = XmlSender(context)
+        return res.deployConfig(context, XMLstr)
+    
+    
+    def createStickiness(self,  context,  vip,  sticky):
+        pass
+    
+    
+    def deleteStickiness(self,  context,  vip,  sticky):
+        pass
+    
     
     def createVIP(self,  context, vip,  sfarm): 
         #access-list permit ip any any - must be added !
@@ -255,47 +373,8 @@ class AceDriver(BaseDriver):
         TMP=TMP+"<loadbalance vip inservice'>\n"
         TMP=TMP+"</class>"
         TMP=TMP+"</policy map>"
-        
-
-
-    def deleteRServer(self, context, rserver):
-        if not bool(rserver.name): 
-            return 'RSERVER NAME ERROR'
-            
-        XMLstr = "<rserver sense='no' name='" + rserver.name + "'></rserver>"
-        
-        s = XmlSender(context)
-        return s.deployConfig(context, XMLstr)    
-        
-        
-        
-    def deleteServerFarm(self,  context,  serverfarm):
-        if not bool(serverfarm.name): 
-            return 'SERVER FARM NAME ERROR'
-
-        XMLstr = "<serverfarm sense='no' name='" + serverfarm.name + "'></serverfarm>"
-        
-        s = XmlSender(context)
-        return s.deployConfig(context, XMLstr) 
-        
-        
-        
-    def activateRServer(self,  context,  serverfarm,  rserver):
-        if not bool(rserver.name): 
-            return 'RSERVER NAME ERROR'
-
-        XMLstr = "<rserver name='" + rserver.name + "'>\r\n  <inservice/>\r\n</rserver>"
-        
-        s = XmlSender(context)
-        return s.deployConfig(context, XMLstr)
-        
-        
-        
-    def suspendRServer(self,  context,  serverfarm,  rserver):
-        if not bool(rserver.name): 
-            return 'RSERVER NAME ERROR'
-
-        XMLstr = "<rserver name='" + rserver.name + "'>\r\n  <inservice sense='no'/>\r\n</rserver>"
-        
-        s = XmlSender(context)
-        return s.deployConfig(context, XMLstr)
+    
+    
+    def deleteVIP(self,  context,  vip):
+        pass
+    
