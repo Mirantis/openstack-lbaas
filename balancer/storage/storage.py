4f5948e71d77bdd43f48ea6f293af8b9e4bce67e
gokrokve <gokrokve@3b54a22f-fe44-4ad7-b819-32ae0e9b6c2a>
2012-04-06 12:04:55 +0000

    Updated keystone config Added files for manual keystone validation
    git-svn-id: svn://192.168.19.148/OpenstackLB/trunk/balancer@370 3b54a22f-fe44-4ad7-b819-32ae0e9b6c2a

--- balancer/storage/storage.py
+++ balancer/storage/storage.py
@@ -592,7 +592,7 @@ class Storage(object):
          if conf == None:
             conf_data = Configuration.Instance()
             conf = conf_data.get()
-            db = conf.db_path
+            db = conf['db_path']
          else:
              db = conf['db_path']
          self._db =db
