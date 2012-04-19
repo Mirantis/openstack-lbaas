0e53758ea9acbea10cc3b90cd48247c554830f09
akyrenys <akyrenys@3b54a22f-fe44-4ad7-b819-32ae0e9b6c2a>
2012-04-19 13:42:00 +0000

    Some fixes to sticky
    git-svn-id: svn://192.168.19.148/OpenstackLB/trunk/balancer@409 3b54a22f-fe44-4ad7-b819-32ae0e9b6c2a

--- balancer/storage/storage.py
+++ balancer/storage/storage.py
@@ -22,6 +22,7 @@ from openstack.common import exception
 from balancer.devices.device import LBDevice
 from balancer.core.configuration import Configuration
 from balancer.loadbalancers.probe import *
+from balancer.loadbalancers.sticky import *
 from balancer.loadbalancers.realserver import RealServer
 from balancer.loadbalancers.predictor import *
 from balancer.loadbalancers.serverfarm import ServerFarm
