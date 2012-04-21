766f3f9417cd3a1e354ee90270e3e6e2eea45b8d
gokrokve <gokrokve@3b54a22f-fe44-4ad7-b819-32ae0e9b6c2a>
2012-04-21 23:04:56 +0000

    1. Added RServer tracking to deploy only one RServer and then use it if someone also using this IP 2. Fixed issues with multiple acl-list deployment 3. Fixed issue with multiple acl-list assign on interface 4. Fixed issue with rollback. Now it finishes
    git-svn-id: svn://192.168.19.148/OpenstackLB/trunk/balancer@428 3b54a22f-fe44-4ad7-b819-32ae0e9b6c2a

--- etc/tables.sql
+++ etc/tables.sql
@@ -2,32 +2,36 @@ CREATE TABLE tenants(id TEXT, name TEXT);
 
 CREATE TABLE tenantdevices(tenant_id TEXT, device_id TEXT);
 
-CREATE TABLE loadbalancers(id TEXT, name TEXT, protocol TEXT, transport TEXT, algorithm TEXT, status TEXT, created TEXT, updated TEXT, device_id TEXT, tenant TEXT);
+CREATE TABLE loadbalancers(id TEXT, name TEXT, protocol TEXT, transport TEXT, algorithm TEXT, status TEXT, created TEXT, updated TEXT, device_id TEXT, tenant TEXT, deployed TEXT);
+CREATE INDEX loadbalancers_id_idx ON loadbalancers (id);
 
 CREATE TABLE serverfarms(id TEXT, lb_id TEXT,  name TEXT,
          type TEXT,  description TEXT, failAction TEXT, inbandHealthCheck TEXT, connFailureThreshCount TEXT,  resetTimeout TEXT,   resumeService TEXT,  transparent TEXT, 
          dynamicWorkloadScale TEXT,  vmProbeName TEXT, failOnAll TEXT,  partialThreshPercentage TEXT,   backInservice TEXT,  sticky TEXT,
-         retcodeMap TEXT,   status TEXT,    created TEXT,  updated TEXT);
+         retcodeMap TEXT,   status TEXT,    created TEXT,  updated TEXT, deployed TEXT);
+CREATE INDEX serverfarms_id_idx ON serverfarms (id);
 
 CREATE TABLE predictors( type TEXT, id TEXT, name TEXT, maskType TEXT, ipNetmask TEXT, ipv6Prefix TEXT, beginPattern TEXT, endPattern TEXT, length TEXT, offsetBytes TEXT,
         cookieName TEXT, customHeader TEXT, definedHeader TEXT, accessTime TEXT, samples TEXT, slowStartDur TEXT, snmpProbe TEXT, autoAdjust TEXT, weightConn TEXT,
-        responseType TEXT, sf_id TEXT);
+        responseType TEXT, sf_id TEXT, deployed TEXT);
+CREATE INDEX predictors_id_idx ON predictors (id);
 
 CREATE TABLE rservers(id TEXT, sf_id TEXT, name TEXT,
          type TEXT, webHostRedir TEXT,   ipType TEXT,   address TEXT,  port TEXT,  state TEXT,   opstate TEXT,    description TEXT,   failOnAll TEXT,   minCon INT,  maxCon INT, 
-         weight INT,  probes TEXT, rateBandwidth INT,   rateConnection INT,    redirectionCode TEXT, backupRS TEXT,  backupRSport TEXT,  created TEXT,  updated TEXT, cookieStr TEXT, status TEXT, condition TEXT, vm_instance TEXT, parent_id TEXT);
-
+         weight INT,  probes TEXT, rateBandwidth INT,   rateConnection INT,    redirectionCode TEXT, backupRS TEXT,  backupRSport TEXT,  created TEXT,  updated TEXT, cookieStr TEXT, status TEXT, condition TEXT, vm_instance TEXT, parent_id TEXT, deployed TEXT);
+CREATE INDEX rservers_id_idx ON rservers (id);
+CREATE INDEX rservers_ip_dep_idx ON rservers (address, deployed);
 
 CREATE TABLE transactions (id TEXT, status TEXT, action TEXT, params TEXT);
 
 CREATE TABLE devices (id TEXT,  name  TEXT, type TEXT, version TEXT, supports_ipv6 INT, requires_vip_ip INT, has_acl INT, supports_vlan INT, ip TEXT, port TEXT, 
-        user TEXT, password TEXT, vip_vlan TEXT, localpath TEXT, configfilepath TEXT, remotepath TEXT, interface TEXT);
+        user TEXT, password TEXT, vip_vlan TEXT, localpath TEXT, configfilepath TEXT, remotepath TEXT, interface TEXT, deployed TEXT);
 
 CREATE TABLE vips(id TEXT, sf_id TEXT, name TEXT, ipVersion TEXT, address TEXT, mask TEXT, proto TEXT, appProto TEXT, port TEXT, allVLANs TEXT, VLAN TEXT,
         connParameterMap  TEXT,  KALAPtagName TEXT,  KALAPprimaryOutOfServ TEXT,  ICMPreply TEXT,  status TEXT,  protocolInspect TEXT,  appAccelAndOpt TEXT,
         L7LoadBalancing TEXT,  lb_id TEXT,  backupServerFarm TEXT,  SSLproxyServName TEXT,  defaultL7LBAction TEXT,  SSLinitiation TEXT,  NAT TEXT,
-        created TEXT,  updated TEXT);
-
+        created TEXT,  updated TEXT, deployed TEXT);
+CREATE INDEX vips_id_idx ON vips (id);
 
 CREATE TABLE vlans(id   TEXT, description   TEXT, intType   TEXT, IPaddr   TEXT, aliasIPaddr   TEXT, peerIPaddr   TEXT, netmask   TEXT, adminStatus   TEXT, 
         enableMACsticky   TEXT, enableNormalization   TEXT, enableIPv6   TEXT, ipv6GlobalIP   TEXT, ipv6UniqueLocalAddr   TEXT, ipv6LinkLocalAddr   TEXT, 
@@ -42,8 +46,10 @@ CREATE TABLE probes(sf_id TEXT,delay TEXT,attemptsBeforeDeactivation TEXT,timeou
         maxExpectStatus TEXT, cipher TEXT, SSLversion TEXT,
         maibox TEXT, requestCommand TEXT,  userSecret TEXT, NASIPaddr TEXT, requareHeaderValue TEXT, proxyRequareHeaderValue TEXT, requestURL TEXT, 
         scriptName TEXT, scriptArgv TEXT,  copied TEXT,  proto TEXT,  sourceFileName TEXT,  SNMPComm TEXT,  SNMPver TEXT,  maxCPUburstThresh TEXT, minCPUburstThresh TEXT,
-        maxMemBurstThresh TEXT, minMemBurstThresh TEXT, VMControllerName TEXT);
-
+        maxMemBurstThresh TEXT, minMemBurstThresh TEXT, VMControllerName TEXT, deployed TEXT);
+CREATE INDEX probes_id_idx ON probes (id);
 CREATE TABLE stickies(id TEXT, sf_id TEXT, name TEXT, type TEXT, serverFarm TEXT, backupServerFarm TEXT, aggregateState TEXT, enableStyckyOnBackupSF TEXT, replicateOnHAPeer TEXT,
         timeout TEXT, timeoutActiveConn TEXT, offset TEXT, length TEXT, beginPattern TEXT, endPattern TEXT, cookieName TEXT, enableInsert TEXT, browserExpire TEXT, secondaryName TEXT,
-        headerName TEXT, netmask TEXT, ipv6PrefixLength TEXT, addressType TEXT, prefixLength TEXT, enableStickyForResponse TEXT, radiusTypes TEXT);
+        headerName TEXT, netmask TEXT, ipv6PrefixLength TEXT, addressType TEXT, prefixLength TEXT, enableStickyForResponse TEXT, radiusTypes TEXT, deployed TEXT);
+CREATE INDEX stickies_id_idx ON stickies (id);
+
