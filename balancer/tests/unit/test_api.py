a3fcafdf6c76feb8ad78be8a28d446672e7d22c8
Nikolaj Starodubtsev <nstarodubtsev@mirantis.com>
2012-06-22 15:49:37 +0400

    Add new tests to core/api.py
    Add tests to device_*, except device_delete - it marked as unused.
    device_create broke with error, cause it use Scheduller.

--- balancer/tests/unit/test_api.py
+++ balancer/tests/unit/test_api.py
@@ -1,7 +1,7 @@
 # vim: tabstop=4 shiftwidth=4 softtabstop=4
 
-from mock import patch, MagicMock as Mock
-from unittest import TestCase
+import mock
+import unittest
 #from balancer.core import commands
 #from balancer.loadbalancers.vserver import Balancer
 #from balancer.devices.DeviceMap import DeviceMap
@@ -10,31 +10,70 @@ from balancer.storage.storage import Storage
 import balancer.core.api as api
 
 
-class WorkWithReader(TestCase):
+class WorkWithReader(unittest.TestCase):
 
     def setUp(self):
-        self.conf = Mock()
-        self.tenant_id = Mock()
-        #self.tenant_id.return_value = "foo"
-        self.store = Mock(spec=Storage(self.conf))
-        #self.store.getReader = Mock()
-        #self.reader = self.store.getReader()
+        self.conf = mock.Mock()
+        self.tenant_id = mock.Mock()
+        self.store = mock.Mock(spec=Storage(self.conf))
 
-    @patch("balancer.db.api.loadbalancer_get_all_by_project")
+    @mock.patch("balancer.db.api.loadbalancer_get_all_by_project")
     def test_lb_get_index(self, mock_api):
         api.lb_get_index(self.conf, self.tenant_id)
         self.assertTrue(mock_api.called)
 
-    @patch("balancer.db.api.loadbalancer_get_all_by_vm_id")
+    @mock.patch("balancer.db.api.loadbalancer_get_all_by_vm_id")
     def test_lb_find_for_vm(self, mock_api):
-        vm_id = Mock()
+        vm_id = mock.Mock()
         api.lb_find_for_vm(self.conf, vm_id, self.tenant_id)
         self.assertTrue(mock_api.called)
 
-    @patch("balancer.db.api.loadbalancer_get")
+    @mock.patch("balancer.db.api.loadbalancer_get")
     def test_get_data(self, mock_api):
-        lb_id = Mock()
+        lb_id = mock.Mock()
         lb_id.return_value = ""
         api.lb_get_data(self.conf, lb_id)
         self.assertTrue(mock_api.called)
-#class WorkWithBalancer(TestCase):
+
+
+#class WorkWithBalancer(unittest.TestCase):
+#    def setUp(self):
+#        self.conf = mock.MagicMock()
+#        self.mock_lb = mock.MagicMock(loadFromDB=mock.MagicMock(),
+#                parseParams=mock.MagicMock)
+
+#    @mock.patch("balancer.loadbalancers.vserver.Balancer")
+#    @mock.patch("balancer.db.api.unpack_extra")
+#    def test_lb_show_details(self, mock_func, mock_obj):
+#        lb_id = mock.MagicMock(spec=int)
+#        mock_obj = self.mock_lb
+#        with mock_obj:
+#            api.lb_show_details(self.conf, lb_id)
+#
+#
+class TestDevice(unittest.TestCase):
+    def setUp(self):
+        self.conf = mock.MagicMock(register_group=mock.MagicMock)
+
+    @mock.patch("balancer.db.api.device_get_all")
+    @mock.patch("balancer.db.api.unpack_extra")
+    def test_device_get_index(self, mock_f1, mock_f2):
+        mock_f2.__iter__.return_value = 1
+        api.device_get_index(self.conf)
+        self.assertTrue(mock_f2.called, "None")
+#        self.assertTrue(mock_f1.called, "None")
+
+    @mock.patch("balancer.db.api.device_pack_extra")
+    @mock.patch("balancer.db.api.device_create")
+    @mock.patch("balancer.core.scheduller.Scheduller")
+    def test_device_create(self, mock_obj, mock_f1, mock_f2):
+        api.device_create(self.conf)
+        self.assertTrue(mock_f2.called, "device_pack_extra not called")
+        self.assertTrue(mock_f1.called, "device_create not called")
+        self.assertTrue(mock_obj.called, "scheduller not called")
+
+    def test_device_info(self):
+        params = {'query_params': 2}
+        res = 1
+        res = api.device_info(params)
+        self.assertEquals(res, None, "Alyarma!")
