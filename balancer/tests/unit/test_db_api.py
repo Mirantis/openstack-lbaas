180f392c568018e054c6683ff2773bffd7cda3cd
Ilya Kharin <akscram@gmail.com>
2012-06-25 18:00:48 +0400

    Complete test device functionality in db.api.

--- balancer/tests/unit/test_db_api.py
+++ balancer/tests/unit/test_db_api.py
@@ -8,6 +8,27 @@ from balancer.db import session
 from balancer import exception
 
 
+device_fake1 = {'name': 'fake1',
+                'type': 'FAKE',
+                'version': '1',
+                'ip': '10.0.0.10',
+                'port': 1010,
+                'user': 'user1',
+                'password': 'secrete1',
+                'extra': {'supports_vlan': False}}
+
+device_fake2 = {'name': 'fake2',
+                'type': 'FAKE',
+                'version': '2',
+                'ip': '10.0.0.20',
+                'port': 2020,
+                'user': 'user2',
+                'password': 'secrete2',
+                'extra': {'supports_vlan': True,
+                          'vip_vlan': 10,
+                          'requires_vip_ip': True}}
+
+
 class TestExtra(unittest.TestCase):
     def test_pack_extra(self):
         model = mock.MagicMock()
@@ -44,48 +65,54 @@ class TestDBAPI(unittest.TestCase):
         os.remove(self.filename)
 
     def test_device_create(self):
-        values = {'name': 'fake1',
-                  'type': 'FAKE',
-                  'version': '1',
-                  'ip': '10.0.0.10',
-                  'port': 2020,
-                  'user': 'user',
-                  'password': 'secrete',
-                  'extra': {'vip_vlan': 75}}
-        device_ref = db_api.device_create(self.conf, values)
+        device_ref = db_api.device_create(self.conf, device_fake1)
         self.assertIsNotNone(device_ref['id'])
-        self.assertEqual(device_ref['name'], values['name'])
-        self.assertEqual(device_ref['type'], values['type'])
-        self.assertEqual(device_ref['version'], values['version'])
-        self.assertEqual(device_ref['ip'], values['ip'])
-        self.assertEqual(device_ref['port'], values['port'])
-        self.assertEqual(device_ref['user'], values['user'])
-        self.assertEqual(device_ref['password'], values['password'])
-        self.assertEqual(device_ref['extra'], values['extra'])
+        self.assertEqual(device_ref['name'], device_fake1['name'])
+        self.assertEqual(device_ref['type'], device_fake1['type'])
+        self.assertEqual(device_ref['version'], device_fake1['version'])
+        self.assertEqual(device_ref['ip'], device_fake1['ip'])
+        self.assertEqual(device_ref['port'], device_fake1['port'])
+        self.assertEqual(device_ref['user'], device_fake1['user'])
+        self.assertEqual(device_ref['password'], device_fake1['password'])
+        self.assertEqual(device_ref['extra'], device_fake1['extra'])
 
     def test_device_update(self):
-        values = {'name': 'fake1',
-                  'type': 'FAKE',
-                  'version': '1',
-                  'ip': '10.0.0.10',
-                  'port': 2020,
-                  'user': 'user',
-                  'password': 'secrete',
-                  'extra': {'vip_vlan': 75}}
-        device_ref = db_api.device_create(self.conf, values)
+        device_ref = db_api.device_create(self.conf, device_fake1)
         self.assertIsNotNone(device_ref['id'])
-        self.assertEqual(device_ref['password'], values['password'])
-        update_values = {'password': 'test',
-                         'extra': {'vip_vlan': 75,
-                                   'requires_vip_ip': 1}}
+        device_update = {'password': 'test',
+                         'extra': {'supports_vlan': True,
+                                   'vip_vlan': 100,
+                                   'requires_vip_ip': True}}
         device_ref = db_api.device_update(self.conf, device_ref['id'],
-                                          update_values)
+                                          device_update)
         self.assertIsNotNone(device_ref['id'])
-        self.assertEqual(device_ref['name'], values['name'])
-        self.assertEqual(device_ref['type'], values['type'])
-        self.assertEqual(device_ref['version'], values['version'])
-        self.assertEqual(device_ref['ip'], values['ip'])
-        self.assertEqual(device_ref['port'], values['port'])
-        self.assertEqual(device_ref['user'], values['user'])
-        self.assertEqual(device_ref['password'], update_values['password'])
-        self.assertEqual(device_ref['extra'], update_values['extra'])
+        self.assertEqual(device_ref['name'], device_fake1['name'])
+        self.assertEqual(device_ref['type'], device_fake1['type'])
+        self.assertEqual(device_ref['version'], device_fake1['version'])
+        self.assertEqual(device_ref['ip'], device_fake1['ip'])
+        self.assertEqual(device_ref['port'], device_fake1['port'])
+        self.assertEqual(device_ref['user'], device_fake1['user'])
+        self.assertEqual(device_ref['password'], device_update['password'])
+        self.assertEqual(device_ref['extra'], device_update['extra'])
+
+    def test_device_get_all(self):
+        device_ref1 = db_api.device_create(self.conf, device_fake1)
+        device_ref2 = db_api.device_create(self.conf, device_fake2)
+        devices = db_api.device_get_all(self.conf)
+        self.assertEqual(len(devices), 2)
+
+    def test_device_get(self):
+        device_ref1 = db_api.device_create(self.conf, device_fake1)
+        device_ref2 = db_api.device_create(self.conf, device_fake2)
+        dev1 = db_api.device_get(self.conf, device_ref1['id'])
+        dev2 = db_api.device_get(self.conf, device_ref2['id'])
+        self.assertEqual(dict(dev1.iteritems()), dict(device_ref1.iteritems()))
+        self.assertEqual(dict(dev2.iteritems()), dict(device_ref2.iteritems()))
+
+    def test_device_delete(self):
+        device_ref = db_api.device_create(self.conf, device_fake1)
+        db_api.device_destroy(self.conf, device_ref['id'])
+        with self.assertRaises(exception.DeviceNotFound) as cm:
+            db_api.device_get(self.conf, device_ref['id'])
+        err = cm.exception
+        self.assertEqual(err.kwargs, {'device_id': device_ref['id']})
