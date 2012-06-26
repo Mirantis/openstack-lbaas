fc0696f4aa865706cc8ff9c65254f7f7228a9bc2
Yuriy Taraday <yorik.sar@gmail.com>
2012-06-26 18:20:58 +0400

    Speedup SQLite in tests

---
+++ balancer/tests/unit/__init__.py
@@ -0,0 +1,6 @@
+from sqlalchemy import pool, event                                                                                                                                                                               
+                                                                                                                                                                                                                 
+@event.listens_for(pool.Pool, 'connect')                                                                                                                                                                        
+def on_connect(dbapi_con, con_record):                                                                                                                                                                           
+    if type(dbapi_con).__module__.startswith('pysqlite'):                                                                                                                                                        
+        dbapi_con.cursor().execute('PRAGMA journal_mode=MEMORY')
